169aa8505832dc1013be4f18adb2e68cab73364a
caojun <991922377@qq.com>
2019-10-09 20:36:19 +0800

    2019.10.9

--- 304.二维区域和检索-矩阵不可变.py
+++ 304.二维区域和检索-矩阵不可变.py
@@ -50,7 +50,14 @@ class NumMatrix(object):
         """
         :type matrix: List[List[int]]
         """
-        
+        if not matrix:
+            return 
+        m=len(matrix)+1
+        n=len(matrix[0])+1
+        self.sums=[[0 for _ in range(n)] for _ in range(m)]
+        for i in range(1,m):
+            for j in range(1,n):
+                self.sums[i][j]=matrix[i-1][j-1]+self.sums[i-1][j]+self.sums[i][j-1]-self.sums[i-1][j-1]
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -60,7 +67,8 @@ class NumMatrix(object):
         :type col2: int
         :rtype: int
         """
-        
+        row1, col1, row2, col2=row1+1,col1+1,row2+1,col2+1
+        return self.sums[row2][col2]-self.sums[row2][col1-1]-self.sums[row1-1][col2]+self.sums[row1-1][col1-1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
